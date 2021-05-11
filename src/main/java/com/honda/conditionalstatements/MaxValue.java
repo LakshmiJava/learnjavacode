10c629f5d6ef560d7d3f55b496b64e9b1fc8dbb6
LakshmiJava <83951352+LakshmiJava@users.noreply.github.com>
2021-05-11 12:09:40 +0530

    Create MaxValue.java

---
+++ src/main/java/com/honda/conditionalstatements/MaxValue.java
@@ -0,0 +1,24 @@
+package com.honda.conditionalstatements;
+public class Maxvalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+
+int res=(a>b)?a:b;
+System.out.println(res);
+
+//max value between three numbers
+
+int x=10;
+int y=40;
+int z=30;
+
+int res1=(x>y)?((x>z)? x:z):((y>z)?y:z);
+System.out.println(res1);
+
+
+	}
+
+}
